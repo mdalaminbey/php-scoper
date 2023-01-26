d6c4cba5735de77c74e04a7b76c782704ffc485c
MD AL AMIN <mdalaminbey@gmail.com>
2023-01-26 23:48:10 +0600

    Update ConsoleScoper.php

--- src/Console/ConsoleScoper.php
+++ src/Console/ConsoleScoper.php
@@ -113,6 +113,9 @@ final class ConsoleScoper
         );
 
         foreach ($files as [$inputFilePath, $inputContents, $outputFilePath]) {
+
+            $outputFilePath =  str_replace('\vendor-src\src', '', $outputFilePath);
+            
             $this->scopeFile(
                 $scoper,
                 $inputFilePath,
